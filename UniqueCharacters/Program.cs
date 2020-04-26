3a2b7ba36fd9c784d27e9ba4830d72143e03862b
Jorge Celorio <jegcelorio@yahoo.com>
2020-04-26 16:02:32 -0500

    Added Linked List, Stack, Queue and Binary Tree

---
+++ UniqueCharacters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UniqueCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = IsUnique("Helo Werld!");
+            Console.WriteLine(result);
+
+
+        }
+
+        private static bool IsUnique(string str)
+        {
+            char ch; 
+
+            if (str.Length > 128)
+                return false;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                ch = str[i];
+                if(str.IndexOf(ch, i + 1) > -1)
+                {
+                    return false;
+                }
+            }
+
+            return true; 
+        }
+    }
+}
